9b813991d14fe5129ae88ba48347a7220fa73fe6
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-08-18 07:02:41 -0400

    update

--- public/app.js
+++ public/app.js
@@ -1,69 +1,69 @@
 //grab articles as json
-$.getJSON("/articles", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        let newDiv = $("<div>");
-        let p = $("<p>");
-        newDiv.attr("data-id", data[i]._id);
-        newDiv.attr("id", "artDiv");
-        let h2 = $("<h2>")
-        h2.addClass("card-title");
-        h2.text(data[i].title)
-        let html = data[i].link;
-        p.text(html)
-        newDiv.append(h2, p)
+$.getJSON("/articles", function(data){
+      for (var i = 0; i < data.length; i++) {
+            let newDiv = $("<div>");
+            let p = $("<p>");
+            newDiv.attr("data-id", data[i]._id);
+            newDiv.attr("id", "artDiv" );
+            let h2 = $("<h2>")
+            h2.addClass("card-title");
+            h2.text(data[i].title)
+            let html = data[i].link;
+            p.text(html)
+            newDiv.append(h2, p)
 
-        $("#articles").append(newDiv);
-        // "<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>"
-    }
+            $("#articles").append(newDiv);
+            // "<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>"
+      }
 });
 
 //ON CLICK P TAG
-$(document).on("click", "#artDiv", function() {
-    $("#notes").empty();
+$(document).on("click", "#artDiv", function(){
+      $("#notes").empty();
 
-    let thisId = $(this).attr("data-id");
+      let thisId = $(this).attr("data-id");
 
-    $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-    }).done(function(data) {
-        console.log(data);
-        // The title of the article
-        $("#notes").append("<h4>" + data.title + "</h4>");
-        // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote' class='btn-large  deep-orange lighten-1' type='submit' name='action'>Save Note</button>");
-        $("#notes").append("<button data-id='" + data._id + "' id='deletenote' class='btn-large  green accent-4' type='submit' name='action'>Delete Note</button>");
+      $.ajax({
+            method:"GET",
+            url: "/articles/" + thisId
+      }).done(function(data){
+                console.log(data);
+                // The title of the article
+                $("#notes").append("<h4>" + data.title + "</h4>");
+                // An input to enter a new title
+                $("#notes").append("<input id='titleinput' name='title' >");
+                // A textarea to add a new note body
+                $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+                // A button to submit a new note, with the id of the article saved to it
+                $("#notes").append("<button data-id='" + data._id + "' id='savenote' class='btn-large  deep-orange lighten-1' type='submit' name='action'>Save Note</button>");
+                $("#notes").append("<button data-id='" + data._id + "' id='deletenote' class='btn-large  green accent-4' type='submit' name='action'>Delete Note</button>");
 
-        if (data.note) {
-            // Place the title of the note in the title input
-            $("#titleinput").val(data.note.title);
-            // Place the body of the note in the body textarea
-            $("#bodyinput").val(data.note.body);
-        }
-    }); //end .done
-}); //end document
+                if (data.note) {
+                      // Place the title of the note in the title input
+                      $("#titleinput").val(data.note.title);
+                      // Place the body of the note in the body textarea
+                      $("#bodyinput").val(data.note.body);
+                }
+      });//end .done
+});//end document
 
 
 //when click saveNOTE button
-$(document).on("click", "#savenote", function() {
-    let thisId = $(this).attr("data-id");
+$(document).on("click", "savenote", function(){
+      let thisId= $(this).attr("data-id");
 
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-            title: $("#titleinput").val(),
-            body: $("#bodyinput").val()
-        }
-    }).done(function(data) {
-        console.log(data);
-        $("#notes").empty();
-    });
+      $.ajax({
+            method:"POST",
+            url: "/aritcles/" + thisId,
+            data: {
+              title: $("#titleinput").val(),
+              body: $("#bodyinput").val()
+            }
+      }).done(function(data){
+            console.log(data);
+            $("#notes").empty();
+      });
 
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
+      $("#titleinput").val("");
+      $("#bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -4,22 +4,22 @@ let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let logger = require("morgan");
 
-//require articles
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-//scraping tools
-let request = require("request");
-let cheerio = require("cheerio");
+ //require articles
+ var Note = require("./models/Note.js");
+ var Article = require("./models/Article.js");
+ //scraping tools
+ let request = require("request");
+ let cheerio = require("cheerio");
 
-mongoose.Promise = Promise;
+ mongoose.Promise = Promise;
 
-//initialize express
-let app = express();
+ //initialize express
+ let app = express();
 
-//morgan and body parser
+ //morgan and body parser
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended:false
 }));
 app.use(express.static("public"));
 
@@ -27,12 +27,12 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/webscrape");
 let db = mongoose.connection;
 //errors
-db.on("error", function(error) {
-    console.log("MOngoose ERROR: ", error);
+db.on("error", function(error){
+  console.log("MOngoose ERROR: ", error);
 });
 
 //log success message
-db.once("open", function() {
+db.once("open", function(){
     console.log("MONGOOSE CONNECT SUCCESS!")
 })
 
@@ -41,81 +41,88 @@ db.once("open", function() {
 // ============================================
 
 
-app.get("/scrape", function(req, res) {
-    request("http://www.echojs.com/", function(error, response, html) {
-        let $ = cheerio.load(html);
-        $("article h2").each(function(i, element) {
-            let result = {};
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
+app.get("/scrape", function(req,res){
+      request("http://www.echojs.com/", function(error,response,html){
+            let $ = cheerio.load(html);
+                //grab h2 in artcile tags and save them as prop of result object
+                $("article h2").each(function(i,element){
+                      let result = {};
+                      result.title = $(this).children("a").text();
+                      result.link = $(this).children("a").attr("href");
 
-            let entry = new Article(result);
+                      let entry = new Article(result);
 
-            entry.save(function(err, doc) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(doc);
-                }
-            }); //end entry.save
-        }); //end .EACH
-    }); //end request
-    res.send("SCRAPE COMPLETED!")
-}); //end app.get
+                      entry.save(function(err,doc){
+                            if (err) {
+                                console.log(err);
+                            }
+                            else{
+                                console.log(doc);
+                            }
+                      });//end entry.save
+                });//end .EACH
+      });//end request
+      res.send("SCRAPE COMPLETED!")
+});//end app.get
 
 
 //get articles scraped from mongodb
-app.get("/articles", function(req, res) {
-    Article.find({}, function(error, doc) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.json(doc);
-        } //end ifelse
-    }); //end find
-}); //end app.get
-
-app.get("/articles/:id", function(req, res) {
-    Article.findOne({ "_id": req.params.id })
-        .populate("note")
-        .exec(function(error, doc) {
+app.get("/articles", function(req,res){
+      Article.find({},function(error,doc){
             if (error) {
                 console.log(error);
             } else {
                 res.json(doc);
-            }
-        });
+            }//end ifelse
+      });//end find
+});//end app.get
+
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
 });
 
 //create a new note or replace existing once
-app.post("/articles/:id", function(req, res) {
-    let newNote = new Note(req.body);
+app.post("/articles/:id", function(req,res){
+      let newNote = new Note(req.body);
 
-    newNote.save(function(error, doc) {
-        if (error) {
-            console.log(error);
-        } else {
-            Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-                .exec(function(err, doc) {
-                    if (err) {
+      newNote.save(function(error,doc){
+            if (error) {
+                console.log(error);
+            } else {
+                Article.findOneAndUpdate({"_id" : req.params.id}, {"note" : doc._id})
+                .exec(function(err,doc){
+                      if (err) {
                         console.log(err);
-                    } else {
+                      } else {
                         res.send(doc);
-                    }
+                      }
 
-                }); //end execute
-        } //end ifelse
-    }); //end newNote.save
-}); //end post
+                });//end execute
+            }//end ifelse
+      });//end newNote.save
+});//end post
 
 
 //delete notes
 
-
-//post notes to DOM
+//delete articles
 
 
 //listen on port
 app.listen(8000, function() {
-    console.log("App running on port 8000!");
+  console.log("App running on port 8000!");
 });
